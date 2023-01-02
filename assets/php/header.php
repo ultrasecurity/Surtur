c88ddb03f07a97f9b44c2e2fb9e83406016efb0d
joshkar <qadir.yolme@gmail.com>
2023-01-02 18:13:44 +0330

    Addition of necessary modules

---
+++ assets/php/header.php
@@ -0,0 +1,14 @@
+<?php
+
+$header = '<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+<h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5>
+<nav class="my-2 my-md-0 mr-md-3">
+  <a class="p-2 text-dark" href="#">Github</a>
+  <a class="p-2 text-dark" href="#">Telegram</a>
+  <a class="p-2 text-dark" href="#">Support</a>
+</nav>
+<a class="btn btn-outline-primary" href="#">Builder</a>
+</div>';
+
+
+?>
